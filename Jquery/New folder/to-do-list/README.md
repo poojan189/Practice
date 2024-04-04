3966bda18bb961be7208556057cce7782b78c587
dhavalpatel6603 <dhaval.studenttdb@gmail.com>
2024-04-04 12:43:05 +0530

    go

---
+++ Jquery/New folder/to-do-list/README.md
@@ -0,0 +1,7 @@
+# To-do list
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/cassie-codes/pen/pYwXwb](https://codepen.io/cassie-codes/pen/pYwXwb).
+
+To do list with plain JS to work out the hurdles before re-making with Vue.js
+
+buttons totes nicked from this *amazing* pen https://codepen.io/nopr/pen/AfHin
---
+++ website 6 freework/pm.js
@@ -0,0 +1,15 @@
+// let a = "HOME";
+// let b ="ABOUT-US";
+// let c ="ISSUES";
+// let d ="ENDORSEMENTS";
+// let e ="CONTACT";
+
+
+
+
+
+// document.getElementById('dh1').innerHTML=a;
+// document.getElementById('dh2').innerHTML=b;
+// document.getElementById('dh3').innerHTML=c;
+// document.getElementById('dh4').innerHTML=d;
+// document.getElementById('dh5').innerHTML=e;
